34417b73ce535721f62008b93334be9662ad4c62
Vedprakash Sharma <vedprakash.sharma246@gmail.com>
2024-07-12 23:16:32 +0530

    change a name key for backend
    change a name of key for backend block blob

--- 10-06-2024/Enviroment/dev/provider.tf
+++ 10-06-2024/Enviroment/dev/provider.tf
@@ -13,7 +13,7 @@ terraform {
     resource_group_name  = "sunita"
     storage_account_name = "sunitastorage"
     container_name       = "sunitacontainer"
-    key                  = "dev.terraform.tfstate"
+    key                  = "development.terraform.tfstate"
   }
 
 }
